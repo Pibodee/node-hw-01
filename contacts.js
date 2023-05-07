34309e17166704e75c9b296c70ca70187b26f47f
Pibodee <mrbodyanuch@gmail.com>
2023-05-07 11:15:36 +0300

    working script 1.0

--- contacts.js
+++ contacts.js
@@ -1,46 +1,46 @@
-const fs = require("fs/promises")
-const path = require("path")
-const crypto = require("crypto")
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
 
-const contactsPath = path.join(process.cwd(), "db", "contacts.json")
+const contactsPath = path.join(process.cwd(), "db", "contacts.json");
 
 const listContacts = async () => {
-    const contacts = await fs.readFile(contactsPath);
-    return JSON.parse(contacts);
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
 };
 
 const getContactById = async (contactId) => {
-    const contacts = await listContacts();
-    const contact = contacts.find((contact) => contact.id === contactId)
-    if (!contact) {
-        throw new Error ("Contact not found")
-    }
-    return contact
-}
-
-const removeContact = async (contactId) => {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((contact) => contact.id === contactId);
-    if (!index) {
-        throw new Error("Contact not found")
-    }
-    contacts.splice(index, 1)
-
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contactId;
-}
-
-const addContact = async (name, email, phone) => {
-    const contacts = await listContacts();
-    const newContact = {
-        id: crypto.randomUUID(),
-        name,
-        email,
-        phone,
-    }
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  if (!contact) {
+    throw new Error("Contact not found");
+  }
+  return contact;
 };
 
-module.exports(listContacts, getContactById, removeContact, addContact)
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (!index) {
+    throw new Error("Contact not found");
+  }
+  contacts.splice(index, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contactId;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
